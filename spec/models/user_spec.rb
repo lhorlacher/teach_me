766e72f3c363c22314a13ea1e75946c5a165e01e
Ryan Horlacher <horlacherr@gmail.com>
2016-08-30 16:16:51 -0600

    user model tested

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe User, type: :model do
 	
   it {should validate_presence_of(:first_name)}
   it {should validate_presence_of(:last_name)}
+  
+  it {should have_many(:lessons).dependent(:destroy)}
 
-  it {should have_many(:lessons) }
+  describe '.teacher?' do
+	  it "returns true if user is a teacher" do
+	  	user = User.create(first_name: 'lonnie', last_name: 'horlacher', email: 'test@test.com', password: 'password', password_confirmation: 'password')
+	  	expect(user.teacher?).to eq(true)
+	  end
+  end
 
-  it "returns true of user is a teacher" do
-  	user = User.create(first_name: 'lonnie', last_name: 'horlacher', email: 'test@test.com', password: 'password', password_confirmation: 'password')
-  	expect(user.teacher?).to eq(true)
+  describe '.student' do 
+  	it 'returns true if user is a student' do 
+  		user = User.create(teacher_id: 1, first_name: 'lonnie', last_name: 'horlacher', email: 'test@test.com', password: 'password', password_confirmation: 'password')
+  		expect(user.student?).to eq(true)
+  	end
   end
 
 end
