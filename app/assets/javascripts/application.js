1423e6527526506c8b32067b446f0f7c848b16e6
Ryan Horlacher <horlacherr@gmail.com>
2016-08-31 16:10:59 -0600

    some stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,10 @@
 //= require turbolinks
 //= require_tree .
 
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+ $(document).ready(function() {
+    $('input#input_text, textarea#textarea1').characterCounter();
+  });
--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe LessonsController, type: :controller do
 
 	desribe 'GET #index' do 
+		it 'returns http success' do 
+		end
 	end
 
 end
