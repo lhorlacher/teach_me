8f3cebb011e458696311a77a4f2603c81a0e272a
Lonnie Horlacher <horlacher.lonnie@gmail.com>
2016-08-31 16:57:17 -0600

    assignments controller spec start

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe AssignmentsController, type: :controller do
 
+	describe "Get #index" do
+		before(:each) do
+			@lesson = FactoryGirl.create(:lesson_with_assignments)
+		it "returns http success" do
+			get :index
+			expect(response.to have_http_staths(:success)
+		end
+
+		it "sets the assignments index variable" do
+		end
+
+		it "returns the correct assignments in the assignments instance variable" do
+			create_list(:assignment, 5)
+			ge
+		end
+
+		it "renders the index template" do
+		end
+
 end
