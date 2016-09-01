e29aaaa668bee8aab855bf2d5d6d8c496647ab99
Ryan Horlacher <horlacherr@gmail.com>
2016-09-01 12:07:33 -0600

    working on lesson testing

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe LessonsController, type: :controller do
 
@@ -21,7 +22,40 @@ RSpec.describe LessonsController, type: :controller do
 		end
 
 		it 'it renders the index template' do 
+			expect(response).to render_template(:index)
 		end
 	end
 
+	describe 'GET #new' do 
+		before(:each) do
+			sign_in(user = FactoryGirl.create(:user))
+			get :new, student_id: user.id
+		end
+
+		it 'returns http success' do 
+			expect(response).to have_http_status(:success)
+		end
+
+		it 'sets the lesson instance variable' do 
+			expect(assigns(:lesson).class).to eq(Lesson)
+		end
+
+		it 'renders the new template' do 
+			expect(response).to render_template(:new)
+		end
+	end
+
+	describe 'POST #create' do 
+		before(:each) do 
+			sign_in(teacher = FactoryGirl.create(:user))
+			student = FactoryGirl.create(:user)
+			lesson_params = {lesson_date: Faker::Date.between(1.month.ago, Date.today),
+			                 notes: 'these are notes', feedback: 'This is feedback'}
+			post :create, {student_id: student.id, lesson: lesson_params}
+		end
+
+		it 'returns http success' do 
+			expect(response).to have_http_status(:success)
+		end
+	end
 end
